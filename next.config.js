31dc471541301f37e91c3680577035a75dd2c1ce
Wilson Neto Dev <contato@wilsonneto.com.br>
2020-03-04 14:33:55 -0300

    Map Paths Ok!

--- next.config.js
+++ next.config.js
@@ -1,13 +1,49 @@
+const { promises: fs } = require('fs');
+
 module.exports = {
-    exportTrailingSlash: true,
-    webpack: (cfg) => {
-        cfg.module.rules.push(
-            {
-                test: /\.md$/,
-                loader: 'frontmatter-markdown-loader',
-                options: { mode: ['body'] }
-            }
-        )
-        return cfg;
-    }
-}
+  exportTrailingSlash: true,
+
+  webpack: cfg => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+      options: { mode: ['body'] }
+    });
+    return cfg;
+  },
+
+  exportPathMap: async () => {
+    const paths = {
+      '/': { page: '/home' },
+      '/about': { page: '/about' },
+      '/posts': { page: '/posts' }
+    };
+
+    /* 
+    await fs.readdir('./_content/posts', (err, files) => {
+      files.forEach(file => {
+        const fileSlug = file.replace('.md', '');
+        console.log(`/post/${fileSlug}`);
+        paths[`/post/${fileSlug}`] = {
+          page: '/post/[slug]',
+          query: { slug: fileSlug }
+        };
+      });
+    });
+    */
+
+    const files = await fs.readdir('./_content/posts');
+    files.forEach(file => {
+      const fileSlug = file.replace('.md', '');
+      console.log(`/post/${fileSlug}`);
+      paths[`/post/${fileSlug}`] = {
+        page: '/post/[slug]',
+        query: { slug: fileSlug }
+      };
+    });
+
+    console.log(paths);
+
+    return paths;
+  }
+};
